4a2dbf755dcd6dcab14ef9c0497b0b4272d31ef8
imanki29 <ilez.mankiev@decathlon.com>
2021-06-25 18:16:19 +0200

    premier commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { ProductListComponent } from 'src/app/products/product-list.component';
+import { FormsModule } from "@angular/forms"
 import { AppComponent } from './app.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent,
+    ConvertToSpacesPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
