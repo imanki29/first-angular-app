7e7ac888087fd5da39fba7a569cabc8a04b2e3e5
imanki29 <ilez.mankiev@decathlon.com>
2021-06-29 16:41:09 +0200

    ajout des images, des routes, d'une navbar et affichage des etoiles et des prix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ProductListComponent } from 'src/app/products/product-list.component';
-import { FormsModule } from "@angular/forms"
+import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
+import { HttpClientModule } from "@angular/common/http";
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from "./home/welcome.component";
+import { RouterModule } from "@angular/router"
 
 @NgModule({
   declarations: [
     AppComponent,
+    WelcomeComponent,
     ProductListComponent,
-    ConvertToSpacesPipe
+    ConvertToSpacesPipe,
+    StarComponent,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      { path: "products", component: ProductListComponent },
+      { path: "products/:id", component: ProductDetailComponent},
+      { path: "welcome", component: WelcomeComponent },
+      { path: "", redirectTo: "welcome", pathMatch: "full" },
+      { path: "**", redirectTo: "welcome", pathMatch: "full" },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,60 +1,59 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
-@Component ({
-    selector: "pm-products",
-    templateUrl: "./product-list.component.html",
-    styleUrls: ["./product-list.component.css"]
+@Component({
+  templateUrl: "./product-list.component.html",
+  styleUrls: ["./product-list.component.css"]
 })
 
-export class ProductListComponent implements OnInit{
-    pageTitle = "Product List";
-    imageWidth = 50;
-    imageMargin = 2;
-    showImage: boolean = false;
-    
-    private _listFilter: string = "";
-    get listFilter(): string {
-        return this._listFilter;
-    }
-    set listFilter(value: string) {
-        this._listFilter = value;
-        console.log("In setter:", value);
-        this.filteredProducts = this.performFilter(value)
-    }
+export class ProductListComponent implements OnInit, OnDestroy {
+  pageTitle = "Product List";
+  imageWidth = 50;
+  imageMargin = 2;
+  showImage: boolean = false;
+  errorMessage: string = "";
+  sub!: Subscription;
 
-    filteredProducts : IProduct[] = [];
-    products: IProduct[] = [
-        {
-      "productId": 2,
-      "productName": "Garden Cart",
-      "productCode": "GDN-0023",
-      "releaseDate": "March 18, 2021",
-      "description": "15 gallon capacity rolling garden cart",
-      "price": 32.99,
-      "starRating": 4.2,
-      "imageUrl": "assets/images/gardenCart.jpg"
-    },
-    {
-      "productId": 5,
-      "productName": "Hammer",
-      "productCode": "TBX-0048",
-      "releaseDate": "May 21, 2021",
-      "description": "Curved claw steel hammer",
-      "price": 8.9,
-      "starRating": 4.8,
-      "imageUrl": "assets/images/hammer.png"
-    }
-    ]
-    performFilter(filterBy: string): IProduct[] {
-        filterBy = filterBy.toLocaleLowerCase();
-        return this.products.filter((product: IProduct) => 
-        product.productName.toLocaleLowerCase().includes(filterBy))
-    }
-    toggleImage(): void {
-        this.showImage = !this.showImage;
-    }
-    ngOnInit(): void {
-        this._listFilter = "cart";
-    }
-}
+  private _listFilter: string = "";
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    console.log("In setter:", value);
+    this.filteredProducts = this.performFilter(value)
+  }
+
+  filteredProducts: IProduct[] = [];
+  products: IProduct[] = [];
+  constructor(private productService: ProductService) {
+
+  }
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().includes(filterBy))
+  }
+  toggleImage(): void {
+    this.showImage = !this.showImage;
+  }
+  ngOnInit(): void {
+    this.sub = this.productService.getProducts().subscribe({
+      next: products => {
+        this.products = products;
+        this.filteredProducts = this.products;
+      },
+      error: err => this.errorMessage = err
+    });
+  };
+
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  };
+
+  onRatingClicked(message: string): void {
+    this.pageTitle = "Product List: " + message;
+  }
+};
